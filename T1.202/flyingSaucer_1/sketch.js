928fbc9c6afdd79e1f3c76f834c81c3c80cbba6e
reincoon <88486053+reincoon@users.noreply.github.com>
2022-04-05 22:27:25 +0100

    Update sketch.js

--- T1.202/flyingSaucer_1/sketch.js
+++ T1.202/flyingSaucer_1/sketch.js
@@ -2,10 +2,29 @@
 //Object orientation revisted
 //part one
 
+var flying_saucer;
+
 function setup()
 {
     createCanvas(800,600);
     noStroke();
+
+    flying_saucer = {
+        x: 400,
+        y: 150,
+        width: 250,
+        height: 50,
+        window_width: 0.5,
+        window_height: 1.2,
+        base_height: 0.45,
+        num_lights: 20,
+        brightnesses: []
+    }
+
+    for(var i = 0; i < flying_saucer.num_lights; i++)
+    {
+        flying_saucer.brightnesses.push((i * 10)%255);
+    }
 }
 
 function draw()
@@ -18,10 +37,39 @@ function draw()
     
     //draw the flying saucer
     fill(175,238,238);
-    arc(200,100,75,100,PI,TWO_PI)
+    arc(flying_saucer.x, 
+        flying_saucer.y, 
+        flying_saucer.width/2,
+        flying_saucer.height * 2,
+        PI,
+        TWO_PI)
     fill(150);
-    arc(200,100,150,50,PI,TWO_PI);
+    arc(flying_saucer.x,
+        flying_saucer.y, 
+        flying_saucer.width,
+        flying_saucer.height,
+        PI,
+        TWO_PI);
     fill(50);
-    arc(200,100,150,25,0,PI);
+    arc(flying_saucer.x,
+        flying_saucer.y,
+        flying_saucer.width,
+        flying_saucer.height/2,
+        0,
+        PI);
     
+    flying_saucer.x += random(-2, 2);
+    flying_saucer.y += random(-1, 1);
+
+    fill(255);
+
+    var incr = flying_saucer.width/(flying_saucer.num_lights - 1);
+    for(var i = 0; i < flying_saucer.num_lights; i++)
+    {
+        fill(flying_saucer.brightnesses[i]);
+        ellipse(flying_saucer.x - flying_saucer.width/2 + incr * i, flying_saucer.y, 5);
+    
+        flying_saucer.brightnesses[i] += 2;
+        flying_saucer.brightnesses[i] = flying_saucer.brightnesses[i]%255;
+    }
 }
