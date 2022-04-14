659e48627d3c36192d100553111db80071969171
reincoon <88486053+reincoon@users.noreply.github.com>
2022-04-14 21:56:23 +0100

    Update lineToTool.js

--- draw-app-CaseStudy/draw-app-CaseStudy/lineToTool.js
+++ draw-app-CaseStudy/draw-app-CaseStudy/lineToTool.js
@@ -1,4 +1,8 @@
 function LineToTool(){
+	// Tool is drawing a straight line from the point where mouse was pressed to the point where mouse is released. 
+	// While mouse is pressed, the function loads the pixel data and displays a flexible line from the start point to the cursor.
+	// Once mouse is released, line is drawn from the start point to the point where mouse was released. 
+	
 	this.icon = "assets/lineTo.jpg";
 	this.name = "LineTo";
 
